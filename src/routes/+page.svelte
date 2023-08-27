4cf7d33664f38e385911f1ffab44d4b9d8280fae
Yeonwu Oh <yeonwu319@gmail.com>
2023-08-27 17:31:47 +0900

    svelte test

---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+    let counter = 0;
+</script>
+
+<svelte:head>
+    <title>Home</title>
+    <meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+    <h1>Welcome!</h1>
+    <button
+        on:click={() => {
+            counter += 1;
+        }}>+</button
+    >
+    <span>{counter}</span>
+    <button
+        on:click={() => {
+            counter -= 1;
+        }}>-</button
+    >
+</section>
+
+<style>
+</style>
